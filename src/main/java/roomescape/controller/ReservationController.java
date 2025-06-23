6cd8a33c2724f412a9126f7062cc0b2dff7fdfba
Mint <audwl03071@gmail.com>
2025-06-23 12:18:29 +0900

    refactor: controller 분리

---
+++ src/main/java/roomescape/controller/ReservationController.java
@@ -0,0 +1,25 @@
+package roomescape.controller;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import roomescape.dto.FindAllReservation;
+import roomescape.service.ReservationService;
+
+@RestController
+@RequestMapping("/reservations")
+public class ReservationController {
+
+    private final ReservationService reservationService;
+
+    public ReservationController(final ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping
+    public List<FindAllReservation> findAll() {
+        return reservationService.findAll();
+    }
+}
